03ca4f5a1792bf03c6a3bdeb93c683957b15d231
Jakub Žitný <jakubzitny@avocode.com>
2019-11-23 23:12:48 +0100

    add hugo server

---
+++ u2net/server.py
@@ -0,0 +1,115 @@
+import hug
+import json
+import base64
+from random import random
+from falcon import HTTP_400, HTTP_200, HTTP_202
+from PIL import Image
+
+from hug.middleware import CORSMiddleware
+import io
+
+from processimage import pil_to_b64, b64_to_pil, process_image
+
+from dramatiq.results import ResultMissing
+
+
+api = hug.API(__name__)
+api.http.add_middleware(CORSMiddleware(api))
+
+
+results = {}
+
+
+def writeFile(data):
+    f = open('files/' + str(random()) + '.png', 'w+b')
+    binary_format = bytearray(data)
+    f.write(binary_format)
+    f.close()
+
+@hug.startup()
+def add_data(api):
+    """Adds initial data to the api on startup"""
+    print("It's working")
+
+
+@hug.post('/upload')
+def main(request, body, response, debug=True):
+    print("Bwah", response)
+    print(type(body))
+    print(len(body))
+
+    decoded = base64.b64decode(body)
+    writeFile(decoded)
+
+    if not body:
+        response.status = HTTP_400
+    print(request)
+    response.status = HTTP_200
+
+@hug.get('/data')
+def data(request, body, response, debug=True):
+    response.status = HTTP_200
+
+    fake_data = [
+        {
+            'id': 1,
+            'name': 'iz181207d0_C1V1_2x1000x',
+            'certainty': 123,
+            'img': 'img',
+        },
+        {
+            'id': 2,
+            'name': 'iz181207d0_C1V1_2x1000x',
+            'certainty': 13,
+            'img': 'img',
+        }
+    ]
+    return json.dumps(fake_data)
+
+@hug.get('/submit')
+def submit(request, body, response, debug=True):
+    response.status = HTTP_200
+
+    im = Image.open("/Users/d_rc/data_playground/sample_data/iz181207d0_C1V1_2x1000x.jpg")
+    result = process_image.send(pil_to_b64(im))
+    mid = result.message_id
+    print(mid)
+
+    results[mid] = result
+
+    return mid
+
+
+@hug.get('/poll')
+def poll(request, body, response, debug=True):
+    response.status = HTTP_200
+
+    err = False
+    resolved_results = []
+    for mid, result in results.items():
+        try:
+            real_result = result.get_result()
+            resolved_result = b64_to_pil(json.loads(real_result)["mask"])
+            resolved_results.append(resolved_result)
+        except ResultMissing as e:
+            err = True
+
+    if (err):
+        response.status = HTTP_202
+        return "processing"
+
+    print(resolved_results)
+
+    return "ok"
+
+# @hug.get('/poll/{id}')
+# def poll(id):
+#     response.status = HTTP_200
+
+
+#     print(id)
+
+#     real_result = results[id].get_result()
+#     b64_to_pil(json.loads(real_result)["mask"])
+
+#     return "ok"
