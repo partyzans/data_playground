96966633fa08c2781a15bd7fb8f7faddbc6d20ca
Jan Tkacik <jan.tkacik@gmail.com>
2019-11-23 22:33:55 +0100

    All data in processed

--- u2net/app.py
+++ u2net/app.py
@@ -1,12 +1,14 @@
 import base64
 import datetime
-from io import BytesIO as _BytesIO
+from io import BytesIO
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from dash.dependencies import Input, Output, State
 from PIL import Image
+
 from processimage import process_image
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -31,6 +33,20 @@ app.layout = html.Div(
             # Allow multiple files to be uploaded
             multiple=True,
         ),
+        dash_table.DataTable(
+            id="output-datatable",
+            columns=[
+                {"name": name, "id": ids}
+                for name, ids in [("Filename", "filename"), ("Message id", "message_id")]
+            ],
+            data=[{"filename": "Test", "message_id": "Test"}],
+            editable=False,
+            sort_action="native",
+            sort_mode="multi",
+            row_selectable="single",
+            row_deletable=True,
+        ),
+        html.Div(id="output-data-table"),
         html.Div(id="output-image-upload"),
     ]
 )
@@ -38,35 +54,40 @@ app.layout = html.Div(
 
 def b64_to_pil(string):
     decoded = base64.b64decode(string)
-    buffer = _BytesIO(decoded)
+    buffer = BytesIO(decoded)
     im = Image.open(buffer)
 
     return im
 
 
+def pil_to_b64(im, enc_format="png", **kwargs):
+    buff = BytesIO()
+    im.save(buff, format=enc_format, **kwargs)
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+
 def parse_contents(contents, filename, date):
-    im_pil = b64_to_pil(contents.split(";base64,")[-1])
+    im_pil = b64_to_pil(contents.split(";base64,")[-1]).resize((512, 384), Image.LANCZOS).convert("RGB")
+    new_content = "data:image/png;base64," + pil_to_b64(im_pil)
     result = process_image.send(contents)
-    # print(result)
-    print(result.message_id)
-    print(type(im_pil))
-    print(result.get_result())
-    return html.Div(
-        [
-            html.H5(filename),
-            html.H6(datetime.datetime.fromtimestamp(date)),
-            # HTML images accept base64 encoded strings in the same format
-            # that is supplied by the upload
-            html.Img(src=contents),
-            html.Hr(),
-            html.Div("Raw Content"),
-            html.Pre(contents[0:200] + "...", style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"}),
-        ]
-    )
+    return {"content_resized": new_content, "filename": filename, "message_id": result.message_id}
+    # return html.Div(
+    #     [
+    #         html.H5(filename),
+    #         html.H6(datetime.datetime.fromtimestamp(date)),
+    #         # HTML images accept base64 encoded strings in the same format
+    #         # that is supplied by the upload
+    #         html.Img(src=new_content),
+    #         html.Hr(),
+    #         html.Div("Raw Content"),
+    #         html.Pre(new_content[0:200] + "...", style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"}),
+    #         html.Div(result.message_id),
+    #     ]
+    # )
 
 
 @app.callback(
-    Output("output-image-upload", "children"),
+    Output("output-datatable", "data"),
     [Input("upload-image", "contents")],
     [State("upload-image", "filename"), State("upload-image", "last_modified")],
 )
@@ -76,6 +97,7 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
         ]
         return children
+    return []
 
 
 if __name__ == "__main__":
